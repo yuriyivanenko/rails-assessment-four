0c22b845c29136e6a4b972d4edcb86ee60c77e0f
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-08-22 10:19:15 -0400

    Add validations to house plants and display errors

--- app/models/house_plant.rb
+++ app/models/house_plant.rb
@@ -1,3 +1,6 @@
 class HousePlant < ApplicationRecord
   belongs_to :room
+  validates :height, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  validates :plant_type, presence: true
+  validates :room, presence: true
 end
