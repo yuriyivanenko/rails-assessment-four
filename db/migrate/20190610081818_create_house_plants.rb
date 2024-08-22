b100f185709199623a3cbc6891301b491bd09fbc
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-08-22 09:24:58 -0400

    Add reference to room for each plant

--- db/migrate/20190610081818_create_house_plants.rb
+++ db/migrate/20190610081818_create_house_plants.rb
@@ -1,6 +1,7 @@
 class CreateHousePlants < ActiveRecord::Migration[5.2]
   def change
     create_table :house_plants do |t|
+      t.references :room, foreign_key: true
       t.string :plant_type
       t.integer :height
     end
